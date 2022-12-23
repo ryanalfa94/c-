bd79a36a1b7bd9513b628c1a1164df2995a92d26
ryanalfa94 <90881035+ryanalfa94@users.noreply.github.com>
2022-12-23 14:58:24 -0700

    updated

--- constrctrs.cpp
+++ constrctrs.cpp
@@ -46,3 +46,10 @@ int main (){
 
     return 0;
 }
+
+/*
+In C++, there are three access specifiers:
+public - members are accessible from outside the class
+private - members cannot be accessed (or viewed) from outside the class
+protected - members cannot be accessed from outside the class, however, they can be accessed in inherited classes. You will learn more about Inheritance later.
+*/
